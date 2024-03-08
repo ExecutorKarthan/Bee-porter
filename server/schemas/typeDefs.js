a195e1deba905fcef2bbe140fe10bd94cc875930
Jared Woodcock <jared.woodcock@yahoo.com>
2024-03-07 21:17:29 -0600

    Rough draft of login/logout/signup functionality

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,10 @@
+const typeDefs = `
+  
+  type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
