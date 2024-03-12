22c404247cca80de1e4cc014284e79e4771d41be
Fleshren <fleshren1@gmail.com>
2024-03-11 21:27:41 -0500

    removed map content from html and integrated into mapComponent.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,11 +14,10 @@ import {
   InMemoryCache,
 } from '@apollo/client';
 
-
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
-}); 
+});
 
 function App() {
   const [count, setCount] = useState(0);
--- client/src/components/mapComponent.jsx
+++ client/src/components/mapComponent.jsx
@@ -2,14 +2,14 @@ import React, { useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
 
 const MapComponent = () => {
-  useEffect(() => {
-    mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
-    const map = new mapboxgl.Map({
-      container: 'map', // container id
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9 // starting zoom
-    });
+    useEffect(() => {
+      mapboxgl.accessToken = 'pk.eyJ1IjoiamZsZXNocmVuIiwiYSI6ImNsdGkyaWRrbzBibHUyaXBuZ3pyOHlpczEifQ.gjpQJcw0X4BCo2d3HGttjQ';
+      const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-74.5, 40], // starting position
+        zoom: 9 // starting zoom
+      });
 
     return () => {
       map.remove();
