6146d6081cf61e5890d4d0d8bc6aebbac102455e
Fleshren <fleshren1@gmail.com>
2024-03-11 20:34:46 -0500

    This should add a functioning map

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <MapComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
