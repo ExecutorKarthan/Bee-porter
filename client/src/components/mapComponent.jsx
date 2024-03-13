19afc23eaffc66bd85e65a3dea809e7ca04345af
Fleshren <fleshren1@gmail.com>
2024-03-13 18:37:09 -0500

    map changes and gitignore modified

--- client/src/components/mapComponent.jsx
+++ client/src/components/mapComponent.jsx
@@ -4,19 +4,30 @@ import mapboxgl from 'mapbox-gl';
 const MapComponent = () => {
     useEffect(() => {
         mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN;
-      const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [-74.5, 40], // starting position
-        zoom: 9 // starting zoom
-      });
 
-    return () => {
-      map.remove();
-    };
-  }, []);
+        if (!navigator.geolocation) {
+            console.error('Geolocation is not supported by your browser');
+            return;
+        }
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const { latitude, longitude } = position.coords;
+                const map = new mapboxgl.Map({
+                    container: 'map',
+                    style: 'mapbox://styles/mapbox/streets-v11',
+                    center: [longitude, latitude], // Set map to center on user's location
+                    zoom: 9 // starting zoom
+                });
 
-  return <div id="map" style={{ width: '100%', height: '400px' }} />;
-};
-
+                return () => {
+                    map.remove();
+                };
+            },
+            (error) => {
+                console.error('Error getting user location:', error);
+            }
+        );
+    }, []);
+    return <div id="map" style={{ width: '100%', height: '400px' }} />;};
+    
 export default MapComponent;
