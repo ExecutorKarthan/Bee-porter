914316871f6b3c3d2df3c10e631151386800cb1e
Fleshren <fleshren1@gmail.com>
2024-03-13 20:51:52 -0500

    added ability to mark map

--- client/src/components/mapComponent.jsx
+++ client/src/components/mapComponent.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 
 const MapComponent = () => {
+    const [map, setMap] = useState(null);
+
     useEffect(() => {
-        mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN
+        mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
         if (!navigator.geolocation) {
             console.error('Geolocation is not supported by your browser');
@@ -12,15 +14,24 @@ const MapComponent = () => {
         navigator.geolocation.getCurrentPosition(
             (position) => {
                 const { latitude, longitude } = position.coords;
-                const map = new mapboxgl.Map({
+                const newMap = new mapboxgl.Map({
                     container: 'map',
                     style: 'mapbox://styles/mapbox/streets-v11',
                     center: [longitude, latitude], // Set map to center on user's location
                     zoom: 9 // starting zoom
                 });
+                setMap(newMap);
+                const marker = new mapboxgl.Marker({
+                    color: '#FF0000',
+                    draggable: true,
+                    // Add custom image URL here
+                    iconUrl: 'IMG URL HERE'
+                })
+                .setLngLat([longitude, latitude]) // Marker position
+                .addTo(newMap); // Add marker to the map
 
                 return () => {
-                    map.remove();
+                    newMap.remove();
                 };
             },
             (error) => {
@@ -28,6 +39,24 @@ const MapComponent = () => {
             }
         );
     }, []);
-    return <div id="map" style={{ width: '100%', height: '400px' }} />;};
-    
+
+    const handleMarkLocation = () => {
+        if (map) {
+            // Get current coordinates of the center of the map
+            const center = map.getCenter();
+            // Add marker at the current coordinates
+            new mapboxgl.Marker()
+                .setLngLat([center.lng, center.lat])
+                .addTo(map);
+        }
+    };
+
+    return (
+        <div>
+            <div id="map" style={{ width: '100%', height: '400px' }} />
+            <button onClick={handleMarkLocation}>Mark Location</button>
+        </div>
+    );
+};
+
 export default MapComponent;
