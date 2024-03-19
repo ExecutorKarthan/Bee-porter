9b9ac7e7742090d44835e2fb1fe3392d4e4043ac
Jared Woodcock <jared.woodcock@yahoo.com>
2024-03-18 21:33:29 -0500

    Styled footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,15 +1,15 @@
 export default function Footer() {
   return (
-    <section>
+    <footer>
       <div className='product'>
         <div className='description'>
           <h3>Help Save the Bees!</h3>
-          <h5>$20.00</h5>
+          <h5>All donations we receive will be sent to <a href="https://www.operationhoneybee.com/" target="_blank" rel="noopener noreferrer" style={{ color: 'white' }}>Operation Honey Bee</a></h5>
         </div>
       </div>
       <form action='/create-checkout-session' method='POST'>
-        <button type="submit" className="button">Donate Here</button>
+        <button type="submit" className="button donate-button">Donate Here</button>
       </form>
-    </section>
+    </footer>
   );
 }
